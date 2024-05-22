a3c8780f852637a6346dc0325750f7f9a31c2e74
SaintPlayzPH <saintplayzph69@gmail.com>
2024-05-22 21:25:28 +0800

    Update WorldRenderer.java

--- src/main/java/net/vulkanmod/render/chunk/WorldRenderer.java
+++ src/main/java/net/vulkanmod/render/chunk/WorldRenderer.java
@@ -318,7 +318,8 @@ public class WorldRenderer {
         Renderer renderer = Renderer.getInstance();
         GraphicsPipeline pipeline = PipelineManager.getTerrainShader(terrainRenderType);
         renderer.bindGraphicsPipeline(pipeline);
-        Renderer.getDrawer().bindAutoIndexBuffer(Renderer.getCommandBuffer(), 7);
+        IndexBuffer indexBuffer = Renderer.getDrawer().getQuadsIndexBuffer().getIndexBuffer();
+        Renderer.getDrawer().bindIndexBuffer(Renderer.getCommandBuffer(), indexBuffer);
 
         int currentFrame = Renderer.getCurrentFrame();
         Set<TerrainRenderType> allowedRenderTypes = Initializer.CONFIG.uniqueOpaqueLayer ? TerrainRenderType.COMPACT_RENDER_TYPES : TerrainRenderType.SEMI_COMPACT_RENDER_TYPES;
