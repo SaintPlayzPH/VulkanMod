2f13b8024fb28126e1138fa309f5e478b757b10c
SaintPlayzPH <saintplayzph69@gmail.com>
2024-05-24 10:46:43 +0800

    Update Options.java

--- src/main/java/net/vulkanmod/config/option/Options.java
+++ src/main/java/net/vulkanmod/config/option/Options.java
@@ -231,6 +231,7 @@ public abstract class Options {
                         new SwitchOption(Component.translatable("Enable Post-Effect"),
                                  value -> {
                                     config.postEffect = value;
+                                    Minecraft.getInstance().delayTextureReload();
                                 },
                                 () -> config.postEffect)
                                 .setTooltip(Component.translatable("Enables Certain Effect 'e.g. Glowing Effect'.")),
