cf17bfa4a8aafa85ffb7254ee7de26f7287f7fdb
SaintPlayzPH <saintplayzph69@gmail.com>
2024-05-24 15:01:01 +0800

    Create LeavesBlockM.java

---
+++ src/main/java/net/vulkanmod/mixin/render/LeavesBlockM.java
@@ -0,0 +1,25 @@
+package net.vulkanmod.mixin.render.leaves;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.LeavesBlock;
+import net.minecraft.util.math.Direction;
+import net.vulkanmod.Initializer;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(value = LeavesBlockM.class, priority = 1900)
+public abstract class MixinLeavesBlock extends Block {
+
+    public LeavesBlockM(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public boolean isSideInvisible(BlockState state, BlockState neighborState, Direction offset) {
+        if (Initializer.CONFIG.cullLeaves) {
+            return neighborState.getBlock() instanceof LeavesBlock;
+        }
+        else return false;
+    }
+}
