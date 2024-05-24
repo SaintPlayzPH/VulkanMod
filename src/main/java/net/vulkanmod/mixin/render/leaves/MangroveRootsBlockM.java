3cb3294f74145dfd9eeb7103ec8a225bec9fe711
SaintPlayzPH <saintplayzph69@gmail.com>
2024-05-24 15:06:50 +0800

    Create MangroveRootsBlockM.java

---
+++ src/main/java/net/vulkanmod/mixin/render/leaves/MangroveRootsBlockM.java
@@ -0,0 +1,23 @@
+package net.vulkanmod.mixin.render.leaves;
+
+import net.minecraft.block.*;
+import net.minecraft.util.math.Direction;
+import net.vulkanmod.Initializer;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(value = MangroveRootsBlockM.class, priority = 1900)
+public abstract class MangroveRootsBlockM extends Block {
+
+    public MangroveRootsBlockM(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public boolean isSideInvisible(BlockState state, BlockState neighborState, Direction offset) {
+        if (Initializer.CONFIG.cullLeaves) {
+            return neighborState.getBlock() instanceof MangroveRootsBlock;
+        }
+        else return false;
+    }
+}
