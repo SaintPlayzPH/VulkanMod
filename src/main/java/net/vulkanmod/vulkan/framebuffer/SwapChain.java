150f2cf69c6127e6dd6be6c98979b496b2839083
SaintPlayzPH <saintplayzph69@gmail.com>
2024-05-25 19:29:38 +0800

    Update SwapChain.java

--- src/main/java/net/vulkanmod/vulkan/framebuffer/SwapChain.java
+++ src/main/java/net/vulkanmod/vulkan/framebuffer/SwapChain.java
@@ -32,7 +32,7 @@ import static org.lwjgl.vulkan.VK10.*;
 
 public class SwapChain extends Framebuffer {
     private static int SWAPCHAIN_IMG_DEDUCTION;
-    private int pretransformFlags = Vulkan.getPretransformFlags();
+    private int pretransformFlagsDat = Vulkan.getPretransformFlags();
     
     // Necessary until tearing-control-unstable-v1 is fully implemented on all GPU Drivers for Wayland
     // (As Immediate Mode (and by extension Screen tearing) doesn't exist on some Wayland installations currently)
@@ -105,7 +105,7 @@ public class SwapChain extends Framebuffer {
             if(Initializer.CONFIG.minImageCount < surfaceProperties.capabilities.minImageCount())
                 Initializer.CONFIG.minImageCount = surfaceProperties.capabilities.minImageCount();
 
-            if (pretransformFlags == VK_SURFACE_TRANSFORM_ROTATE_90_BIT_KHR || pretransformFlags == VK_SURFACE_TRANSFORM_ROTATE_270_BIT_KHR) {
+            if (pretransformFlagsDat == VK_SURFACE_TRANSFORM_ROTATE_90_BIT_KHR || pretransformFlagsDat == VK_SURFACE_TRANSFORM_ROTATE_270_BIT_KHR) {
                 SWAPCHAIN_IMG_DEDUCTION = 2;
             } else {
                 SWAPCHAIN_IMG_DEDUCTION = 1;
