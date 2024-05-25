cb7b792831e42742e966001fff652c9db94b088e
SaintPlayzPH <saintplayzph69@gmail.com>
2024-05-25 14:07:25 +0800

    Update TintCache.java

--- src/main/java/net/vulkanmod/render/chunk/build/TintCache.java
+++ src/main/java/net/vulkanmod/render/chunk/build/TintCache.java
@@ -6,6 +6,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.util.Mth;
 import net.minecraft.world.level.ColorResolver;
 import net.minecraft.world.level.Level;
+import net.vulkanmod.Initializer;
 import net.minecraft.world.level.biome.Biome;
 import net.vulkanmod.render.chunk.WorldRenderer;
 import net.vulkanmod.render.chunk.build.biome.BoxBlur;
@@ -45,7 +46,7 @@ public class TintCache {
 
         int size = totalWidth * totalWidth;
 
-        if(size != dataSize) {
+        if (size != dataSize) {
             this.dataSize = size;
             for (Layer layer : layers) {
                 layer.allocate(size);
@@ -61,7 +62,7 @@ public class TintCache {
     public int getColor(BlockPos blockPos, ColorResolver colorResolver) {
         int relY = blockPos.getY() & 15;
         Layer layer = layers[relY];
-        if(layer.invalidated)
+        if (layer.invalidated)
             calculateLayer(relY);
 
         int[] values = layer.getValues(colorResolver);
@@ -72,6 +73,14 @@ public class TintCache {
     }
 
     public void calculateLayer(int y) {
+        if (Initializer.CONFIG.tintBuilder == 1) {
+            calculateLayerV1(y);
+        } else if (Initializer.CONFIG.tintBuilder == 2) {
+            calculateLayerV2(y);
+        }
+    }
+
+    private void calculateLayerV1(int y) {
         int absY = (secY << 4) + y;
         Level level = WorldRenderer.getLevel();
         Layer layer = layers[y];
@@ -84,22 +93,18 @@ public class TintCache {
         mixedBiomes |= biome != level.getBiome(blockPos.set(minX, absY, maxZ)).value();
         mixedBiomes |= biome != level.getBiome(blockPos.set(maxX, absY, maxZ)).value();
 
-        // Most sections are completely contained inside a single biome
-        // hence there's no need to do expensive biome calculations
-        if(!mixedBiomes) {
-            for (int absX = minX; absX < maxX ; absX++) {
-                for (int absZ = minZ; absZ < maxZ ; absZ++) {
-
+        if (!mixedBiomes) {
+            for (int absX = minX; absX < maxX; absX++) {
+                for (int absZ = minZ; absZ < maxZ; absZ++) {
                     final int idx = (absX - minX) + (absZ - minZ) * totalWidth;
                     layer.grass[idx] = biome.getGrassColor(absX, absZ);
                     layer.foliage[idx] = biome.getFoliageColor();
                     layer.water[idx] = biome.getWaterColor();
                 }
             }
-        }
-        else {
-            for (int absX = minX; absX < maxX ; absX++) {
-                for (int absZ = minZ; absZ < maxZ ; absZ++) {
+        } else {
+            for (int absX = minX; absX < maxX; absX++) {
+                for (int absZ = minZ; absZ < maxZ; absZ++) {
                     blockPos.set(absX, absY, absZ);
                     biome = level.getBiome(blockPos).value();
 
@@ -110,11 +115,44 @@ public class TintCache {
                 }
             }
 
-            if(blendRadius > 0) {
+            if (blendRadius > 0) {
                 BoxBlur.blur(layer.grass, temp, SECTION_WIDTH, blendRadius);
                 BoxBlur.blur(layer.foliage, temp, SECTION_WIDTH, blendRadius);
                 BoxBlur.blur(layer.water, temp, SECTION_WIDTH, blendRadius);
             }
+        }
+
+        layer.invalidated = false;
+    }
+
+    private void calculateLayerV2(int y) {
+        Level level = WorldRenderer.getLevel();
+        Layer layer = layers[y];
+
+        BlockPos.MutableBlockPos blockPos = new BlockPos.MutableBlockPos();
+        int absY = (secY << 4) + y + level.getMinBuildHeight();
+
+        Biome tB = level.getBiome(blockPos.set(minX, absY, minZ)).value();
+        boolean mixed = false;
+        for (int absZ = minZ; absZ < maxZ; absZ++) {
+            for (int absX = minX; absX < maxX; absX++) {
+                blockPos.set(absX, absY, absZ);
+                Biome biome = level.getBiome(blockPos).value();
+
+                if (biome != tB)
+                    mixed = true;
+
+                final int idx = (absX - minX) + (absZ - minZ) * totalWidth;
+                layer.grass[idx] = biome.getGrassColor(absX, absZ);
+                layer.foliage[idx] = biome.getFoliageColor();
+                layer.water[idx] = biome.getWaterColor();
+            }
+        }
+
+        if (mixed && blendRadius > 0) {
+            BoxBlur.blur(layer.grass, temp, SECTION_WIDTH, blendRadius);
+            BoxBlur.blur(layer.foliage, temp, SECTION_WIDTH, blendRadius);
+            BoxBlur.blur(layer.water, temp, SECTION_WIDTH, blendRadius);
         }
 
         layer.invalidated = false;
@@ -139,14 +177,14 @@ public class TintCache {
         }
 
         public int[] getValues(ColorResolver colorResolver) {
-            if(colorResolver == BiomeColors.GRASS_COLOR_RESOLVER)
+            if (colorResolver == BiomeColors.GRASS_COLOR_RESOLVER)
                 return grass;
-            else if(colorResolver == BiomeColors.FOLIAGE_COLOR_RESOLVER)
+            else if (colorResolver == BiomeColors.FOLIAGE_COLOR_RESOLVER)
                 return foliage;
-            else if(colorResolver == BiomeColors.WATER_COLOR_RESOLVER)
+            else if (colorResolver == BiomeColors.WATER_COLOR_RESOLVER)
                 return water;
 
             throw new IllegalArgumentException("Unexpected resolver: " + colorResolver.toString());
         }
     }
-            }
+}
