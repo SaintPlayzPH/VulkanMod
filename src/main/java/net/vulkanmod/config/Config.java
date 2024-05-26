28d14dfdb57a2980d24d817a9aa637b6e8086a44
SaintPlayzPH <saintplayzph69@gmail.com>
2024-05-26 16:52:41 +0800

    Update Config.java

--- src/main/java/net/vulkanmod/config/Config.java
+++ src/main/java/net/vulkanmod/config/Config.java
@@ -33,7 +33,7 @@ public class Config {
     public boolean glowEffectFix = true;
     public boolean renderFog = true;
     public int tintBuilder = 1;
-    public int minImageCount = 3;
+    public int imageCount = 3;
     public int device = -1;
 
     public int ambientOcclusion = 1;
