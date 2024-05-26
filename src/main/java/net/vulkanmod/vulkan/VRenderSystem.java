aa1c18361c2726c515e544e03dba74351900e59f
SaintPlayzPH <saintplayzph69@gmail.com>
2024-05-26 10:12:16 +0800

    Update VRenderSystem.java

--- src/main/java/net/vulkanmod/vulkan/VRenderSystem.java
+++ src/main/java/net/vulkanmod/vulkan/VRenderSystem.java
@@ -40,20 +40,20 @@ public abstract class VRenderSystem {
     public static final float clearDepth = 1.0f;
     public static FloatBuffer clearColor = MemoryUtil.memCallocFloat(4);
 
-    public static MappedBuffer modelViewMatrix = new MappedBuffer(16 * 4);
-    public static MappedBuffer projectionMatrix = new MappedBuffer(16 * 4);
-    public static MappedBuffer TextureMatrix = new MappedBuffer(16 * 4);
-    public static MappedBuffer MVP = new MappedBuffer(16 * 4);
+    public static final MappedBuffer modelViewMatrix = MappedBuffer.getMappedBuffer(16 * 4);
+    public static final MappedBuffer projectionMatrix = MappedBuffer.getMappedBuffer(16 * 4);
+    public static final MappedBuffer TextureMatrix = MappedBuffer.getMappedBuffer(16 * 4);
+    public static final MappedBuffer MVP = MappedBuffer.getMappedBuffer(16 * 4);
 
-    public static MappedBuffer ChunkOffset = new MappedBuffer(3 * 4);
-    public static MappedBuffer lightDirection0 = new MappedBuffer(3 * 4);
-    public static MappedBuffer lightDirection1 = new MappedBuffer(3 * 4);
+    public static final MappedBuffer ChunkOffset = MappedBuffer.getMappedBuffer(3 * 4);
+    public static final MappedBuffer lightDirection0 = MappedBuffer.getMappedBuffer(3 * 4);
+    public static final MappedBuffer lightDirection1 = MappedBuffer.getMappedBuffer(3 * 4);
 
-    public static MappedBuffer shaderColor = new MappedBuffer(4 * 4);
-    public static MappedBuffer shaderFogColor = new MappedBuffer(4 * 4);
+    public static final MappedBuffer shaderColor = MappedBuffer.getMappedBuffer(4 * 4);
+    public static final MappedBuffer shaderFogColor = MappedBuffer.getMappedBuffer(4 * 4);
 
-    public static MappedBuffer screenSize = new MappedBuffer(2 * 4);
-
+    public static final MappedBuffer screenSize = MappedBuffer.getMappedBuffer(2 * 4);
+    
     public static float alphaCutout = 0.0f;
 
     private static final float[] depthBias = new float[2];
