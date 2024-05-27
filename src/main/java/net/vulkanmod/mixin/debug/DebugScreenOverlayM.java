5a4c147695b64ea326dfbe288ba6a2a9b96614f1
SaintPlayzPH <saintplayzph69@gmail.com>
2024-05-27 08:38:07 +0800

    Update DebugScreenOverlayM.java

--- src/main/java/net/vulkanmod/mixin/debug/DebugScreenOverlayM.java
+++ src/main/java/net/vulkanmod/mixin/debug/DebugScreenOverlayM.java
@@ -66,7 +66,7 @@ public abstract class DebugScreenOverlayM {
             proc = true;
         }
 
-        strings.add(String.format("Java: %s %dbit", System.getProperty("java.version"), this.minecraft.is64Bit() ? 64 : 32));
+        strings.add(String.format("Java: %s %dbit", System.getProperty("java.version"), 64));
         strings.add(String.format("Mem: % 2d%% %03d/%03dMB", usedMemory * 100L / maxMemory, bytesToMegabytes(usedMemory), bytesToMegabytes(maxMemory)));
         strings.add(String.format("Allocated: % 2d%% %03dMB", totalMemory * 100L / maxMemory, bytesToMegabytes(totalMemory)));
         strings.add(String.format("Off-heap: " + getOffHeapMemory() + "MB"));
