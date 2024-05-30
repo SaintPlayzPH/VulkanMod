0db78aa04df18baca6bd2ad54311fa274d3631ca
SaintPlayzPH <saintplayzph69@gmail.com>
2024-05-30 17:02:54 +0800

    Update IndirectBuffer.java

--- src/main/java/net/vulkanmod/vulkan/memory/IndirectBuffer.java
+++ src/main/java/net/vulkanmod/vulkan/memory/IndirectBuffer.java
@@ -4,14 +4,14 @@ import net.vulkanmod.vulkan.Synchronization;
 import net.vulkanmod.vulkan.Vulkan;
 import net.vulkanmod.vulkan.device.DeviceManager;
 import net.vulkanmod.vulkan.queue.CommandPool;
+import net.vulkanmod.vulkan.queue.Queue;
 
 import java.nio.ByteBuffer;
 
-import static net.vulkanmod.vulkan.queue.Queue.TransferQueue;
 import static org.lwjgl.vulkan.VK10.VK_BUFFER_USAGE_INDIRECT_BUFFER_BIT;
 
 public class IndirectBuffer extends Buffer {
-    CommandPool.CommandBuffer commandBuffer;
+    private CommandPool.CommandBuffer commandBuffer;
 
     public IndirectBuffer(int size, MemoryType type) {
         super(VK_BUFFER_USAGE_INDIRECT_BUFFER_BIT, type);
@@ -22,42 +22,43 @@ public class IndirectBuffer extends Buffer {
         int size = byteBuffer.remaining();
 
         if (size > this.bufferSize - this.usedBytes) {
-            resizeBuffer();
+            resizeBuffer(size);
         }
 
         if (this.type.mappable()) {
             this.type.copyToBuffer(this, size, byteBuffer);
         } else {
-            if (commandBuffer == null)
-                commandBuffer = DeviceManager.getTransferQueue().beginCommands();
-
+            ensureCommandBufferInitialized();
             StagingBuffer stagingBuffer = Vulkan.getStagingBuffer();
             stagingBuffer.copyBuffer(size, byteBuffer);
-
-            TransferQueue.uploadBufferCmd(commandBuffer.getHandle(), stagingBuffer.id, stagingBuffer.offset, this.getId(), this.getUsedBytes(), size);
+            Queue.TransferQueue.uploadBufferCmd(commandBuffer.getHandle(), stagingBuffer.id, stagingBuffer.offset, this.getId(), this.getUsedBytes(), size);
         }
 
-        offset = usedBytes;
-        usedBytes += size;
+        this.usedBytes += size;
     }
 
-    private void resizeBuffer() {
+    private void ensureCommandBufferInitialized() {
+        if (commandBuffer == null) {
+            commandBuffer = DeviceManager.getTransferQueue().beginCommands();
+        }
+    }
+
+    private void resizeBuffer(int requiredSize) {
+        int newSize = Math.max(this.bufferSize + (this.bufferSize >> 1), requiredSize);
         MemoryManager.getInstance().addToFreeable(this);
-        int newSize = this.bufferSize + (this.bufferSize >> 1);
         this.createBuffer(newSize);
         this.usedBytes = 0;
     }
 
     public void submitUploads() {
-        if (commandBuffer == null)
-            return;
-
-        DeviceManager.getTransferQueue().submitCommands(commandBuffer);
-        Synchronization.INSTANCE.addCommandBuffer(commandBuffer);
-        commandBuffer = null;
+        if (commandBuffer != null) {
+            Queue.TransferQueue.submitCommands(commandBuffer);
+            Synchronization.INSTANCE.addCommandBuffer(commandBuffer);
+            commandBuffer = null;
+        }
     }
 
-    //debug
+    // Debug method to get ByteBuffer
     public ByteBuffer getByteBuffer() {
         return this.data.getByteBuffer(0, this.bufferSize);
     }
