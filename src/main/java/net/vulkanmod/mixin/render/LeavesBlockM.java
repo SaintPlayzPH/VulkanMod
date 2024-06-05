90c6b6feff318f0fada60043ca1afad75d36a5c5
SaintPlayzPH <saintplayzph69@gmail.com>
2024-06-05 08:49:52 +0800

    Update LeavesBlockM.java

--- src/main/java/net/vulkanmod/mixin/render/LeavesBlockM.java
+++ src/main/java/net/vulkanmod/mixin/render/LeavesBlockM.java
@@ -1,7 +1,9 @@
 package net.vulkanmod.mixin.render;
 
-import net.minecraft.block.BlockState;
-import net.minecraft.block.LeavesBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.LeavesBlock;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -10,8 +12,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(LeavesBlock.class)
 public abstract class LeavesBlockMixin {
 
-    @Inject(method = "isOpaque", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "method_9568", at = @At("HEAD"), cancellable = true)
     private void makeNonTranslucent(BlockState state, CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(true);
+    }
+
+    @Inject(method = "method_9588", at = @At("HEAD"), cancellable = true)
+    private void makeFullCube(BlockState state, BlockGetter world, BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
         cir.setReturnValue(true);
     }
 }
