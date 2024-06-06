b7d7d823eaabf3fc7951a5f4ebe89aa10f497800
SaintPlayzPH <saintplayzph69@gmail.com>
2024-06-06 19:34:42 +0800

    Update Options.java

--- src/main/java/net/vulkanmod/config/option/Options.java
+++ src/main/java/net/vulkanmod/config/option/Options.java
@@ -424,6 +424,10 @@ public abstract class Options {
                     value -> config.showlowRAM = value,
                     () -> config.showlowRAM)
                     .setTooltip(Component.translatable("vulkanmod.options.showLowRAM.tooltip")),
+            new SwitchOption(Component.translatable("vulkanmod.options.resetHighUsageRec"),
+                    value -> config.resetHighUsageRec = value,
+                    () -> config.resetHighUsageRec)
+                    .setTooltip(Component.translatable("vulkanmod.options.resetHighUsageRec.tooltip")),
             new CyclingOption<>(Component.translatable("vulkanmod.options.deviceSelector"),
                     IntStream.range(-1, DeviceManager.suitableDevices.size()).boxed().toArray(Integer[]::new),
                     value -> config.device = value,
