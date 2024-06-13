e86973d9d1f484e78aa2876fe91dd942672f3cbd
Shadow <saintplayzph69@gmail.com>
2024-06-13 09:49:25 +0800

    Update Booleans.java

--- src/main/java/net/vulkanmod/vulkan/Booleans.java
+++ src/main/java/net/vulkanmod/vulkan/Booleans.java
@@ -1,7 +1,14 @@
 package net.vulkanmod.vulkan;
 
-import net.minecraft.client.Minecraft;
+import net.minecraft.client.*;
 
 public class Booleans {
-    public static boolean fancyGraphics = Minecraft.useFancyGraphics();
+    private static Minecraft minecraft = Minecraft.getInstance();
+    private static net.minecraft.client.Options minecraftOptions = minecraft.options;
+
+    // public static boolean fancyGraphics = Minecraft.useFancyGraphics();
+
+    public static boolean isVsyncEnabled() {
+        return minecraftOptions.enableVsync();
+    }
 }
