7596701f0a254bafdf57c20a8b45028f1c3f04b7
Shadow <saintplayzph69@gmail.com>
2024-06-14 17:25:52 +0800

    Update SystemInfo.java

--- src/main/java/net/vulkanmod/vulkan/SystemInfo.java
+++ src/main/java/net/vulkanmod/vulkan/SystemInfo.java
@@ -2,54 +2,49 @@ package net.vulkanmod.vulkan;
 
 import net.vulkanmod.Initializer;
 import oshi.hardware.CentralProcessor;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class SystemInfo {
-    private static boolean procObtain = false;
-    private static boolean cpuObtain = false;
+    private static boolean procObtained = false;
+    private static boolean cpuObtained = false;
     public static final String cpuInfo;
 
     static {
-        boolean isAndroid = isRunningOnAndroid();
-        if (isAndroid) {
-            if (!procObtain) {
-                Initializer.LOGGER.info("Obtaining Processor Name on your Device since you're running on Mobile!");
-                procObtain = true;
-            }
-            cpuInfo = getProcessorNameForAndroid();
-        } else {
-            if (!cpuObtain) {
-                Initializer.LOGGER.info("Obtaining CPU Name on your Device!");
-                cpuObtain = true;
-            }
-            CentralProcessor centralProcessor = new oshi.SystemInfo().getHardware().getProcessor();
-            cpuInfo = centralProcessor.getProcessorIdentifier().getName().replaceAll("\\s+", " ");
+        cpuInfo = isRunningOnAndroid() ? getProcessorNameForAndroid() : getProcessorNameForDesktop();
+    }
+
+    private static String getProcessorNameForAndroid() {
+        if (!procObtained) {
+            Initializer.LOGGER.info("Obtaining Processor Name on your Device since you're running on Mobile!");
+        }
+
+        try (BufferedReader br = new BufferedReader(new FileReader("/proc/cpuinfo"))) {
+            return br.lines()
+                    .filter(line -> line.startsWith("Hardware"))
+                    .map(line -> line.split(":\\s+", 2)[1])
+                    .findFirst()
+                    .orElse("Unknown");
+        } catch (IOException e) {
+            Initializer.LOGGER.error("Can't obtain your Mobile processor name!", e);
+            return "Unknown";
         }
     }
 
-    public static String getProcessorNameForAndroid() {
-        try (BufferedReader br = new BufferedReader(new FileReader("/proc/cpuinfo"))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                if (line.startsWith("Hardware")) {
-                    return line.split(":\\s+", 2)[1];
-                }
-            }
-        } catch (IOException e) {
-            Initializer.LOGGER.error("Can't obtain your Mobile processor name!");
+    private static String getProcessorNameForDesktop() {
+        if (!cpuObtained) {
+            Initializer.LOGGER.info("Obtaining CPU Name on your Device!");
         }
-        return "Unknown";
+
+        var centralProcessor = new oshi.SystemInfo().getHardware().getProcessor();
+        return centralProcessor.getProcessorIdentifier().getName().replaceAll("\\s+", " ");
     }
 
     private static boolean isRunningOnAndroid() {
-        if (System.getenv("POJAV_ENVIRON") != null) { //PojavLauncher
-            return true;
-        }
-        if (System.getenv("SCL_ENVIRON") != null) { //SolCraftLauncher
-            return true;
-        }
-        return System.getenv("POJAV_RENDERER") != null;
+        return System.getenv("POJAV_ENVIRON") != null ||
+               System.getenv("SCL_ENVIRON") != null ||
+               System.getenv("POJAV_RENDERER") != null;
     }
 }
