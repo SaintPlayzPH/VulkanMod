e4a9478117f13afeab89084a5790dc61a3ae8a78
Shadow <saintplayzph69@gmail.com>
2024-06-18 06:42:35 +0800

    Update PostPassM.java

--- src/main/java/net/vulkanmod/mixin/compatibility/PostPassM.java
+++ src/main/java/net/vulkanmod/mixin/compatibility/PostPassM.java
@@ -73,7 +73,7 @@ public class PostPassM {
         VRenderSystem.disableCull();
         RenderSystem.depthFunc(519);
 
-        Renderer.setViewport2(0, this.outTarget.height, this.outTarget.width, -this.outTarget.height);
+        Renderer.setViewport2(0, this.outTarget.width, this.outTarget.height, -this.outTarget.width);
         Renderer.resetScissor();
 
         this.effect.apply();
