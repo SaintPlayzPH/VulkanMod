5c03d1dc9f80dbe8b8b98c9e3e17749de6b882e5
Shadow <saintplayzph69@gmail.com>
2024-06-17 08:04:46 +0800

    Update Initializer.java

--- src/main/java/net/vulkanmod/Initializer.java
+++ src/main/java/net/vulkanmod/Initializer.java
@@ -28,11 +28,11 @@ public class Initializer implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         if (System.getenv("POJAV_ENVIRON") != null) {
-       	    LOGGER.info("=> We're running on PojavLauncher! <=");
-	}
-	if (System.getenv("SCL_ENVIRON") != null) {
-       	    LOGGER.info("=> We're running on SolCraftLauncher! <=");
-	}
+            LOGGER.info("=> We're running on PojavLauncher! <=");
+        }
+        if (System.getenv("SCL_ENVIRON") != null) {
+            LOGGER.info("=> We're running on SolCraftLauncher! <=");
+        }
         VERSION = FabricLoader.getInstance()
                 .getModContainer("vulkanmod")
                 .get()
@@ -48,7 +48,10 @@ public class Initializer implements ClientModInitializer {
         }
         Platform.init();
         VideoModeManager.init();
-	Renderer.recompile = true;
+        
+        // Recompile Renderer and log config
+        Renderer.recompile = true;
+        logConfig(CONFIG);
     }
 
     private static void initializeConfig() {
@@ -76,5 +79,34 @@ public class Initializer implements ClientModInitializer {
 
     public static String getVersion() {
         return VERSION;
+    }
+
+    private static void logConfig(Config config) {
+        LOGGER.info("Frame Queue Size: " + config.frameQueueSize);
+        LOGGER.info("Show Device RAM: " + config.showDeviceRAM);
+        LOGGER.info("Show Pojav Info: " + config.pojavInfo);
+        LOGGER.info("Advanced Culling: " + config.advCulling);
+        LOGGER.info("Indirect Draw: " + config.indirectDraw);
+        LOGGER.info("Low VRAM Mode: " + config.perRenderTypeAreaBuffers);
+        LOGGER.info("Fast Leaves Fix: " + config.fastLeavesFix);
+        LOGGER.info("Entity Culling: " + config.entityCulling);
+        LOGGER.info("Animations: " + config.animations);
+        LOGGER.info("Render Sky: " + config.renderSky);
+        LOGGER.info("Render Sky Fog: " + config.renderSkyFog);
+        LOGGER.info("Render Cloud Fog: " + config.renderCloudFog);
+        LOGGER.info("Post-effect Fix: " + config.postEffectFix);
+        LOGGER.info("Render Fog: " + config.renderFog);
+        LOGGER.info("Entity Outline: " + config.entityOutline);
+        LOGGER.info("Exclude Sampled Usage: " + config.dontUseImageSampled);
+        LOGGER.info("Reset Highest Usage Records: " + config.resetHighUsageRec);
+        LOGGER.info("Show Low RAM Warning: " + config.showlowRAM);
+        LOGGER.info("Faster Gaussian Sky Blending: " + config.gaussianSkyBlending);
+        LOGGER.info("Disable Depth Write if Translucent: " + config.depthWrite);
+        LOGGER.info("Force FIFO VSync: " + config.forceFIFOVsync);
+        LOGGER.info("Present Mode: " + config.presentMode);
+        LOGGER.info("RAM Info Update: " + config.ramInfoUpdate);
+        LOGGER.info("Image Count: " + config.imageCount);
+        LOGGER.info("Device: " + config.device);
+        LOGGER.info("Ambient Occlusion: " + config.ambientOcclusion);
     }
 }
