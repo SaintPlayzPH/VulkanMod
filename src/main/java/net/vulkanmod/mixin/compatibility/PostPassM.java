ecfcfde01ed0488f2b6c19548076aa109dc796bd
Shadow <saintplayzph69@gmail.com>
2024-06-24 14:33:59 +0800

    Add files via upload

--- src/main/java/net/vulkanmod/mixin/compatibility/PostPassM.java
+++ src/main/java/net/vulkanmod/mixin/compatibility/PostPassM.java
@@ -1,15 +1,11 @@
 package net.vulkanmod.mixin.compatibility;
 
-import com.mojang.blaze3d.pipeline.MainTarget;
 import com.mojang.blaze3d.pipeline.RenderTarget;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.*;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.EffectInstance;
 import net.minecraft.client.renderer.PostPass;
-import net.vulkanmod.Initializer;
-import net.vulkanmod.vulkan.Renderer;
-import net.vulkanmod.vulkan.VRenderSystem;
 import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -40,51 +36,44 @@ public class PostPassM {
     @Shadow private Matrix4f shaderOrthoMatrix;
 
     /**
-     * Overwrite the process method to correctly handle viewport and scissor settings.
+     * @author
+     * @reason
      */
     @Overwrite
     public void process(float f) {
         this.inTarget.unbindWrite();
-        float g = (float)this.outTarget.height; // swapped width with height
-        float h = (float)this.outTarget.width;  // swapped width with height
-        RenderSystem.viewport(0, 0, (int)h, (int)g); // swapped width with height
+        float g = (float)this.outTarget.width;
+        float h = (float)this.outTarget.height;
+        RenderSystem.viewport(0, 0, (int)g, (int)h);
 
         Objects.requireNonNull(this.inTarget);
         this.effect.setSampler("DiffuseSampler", this.inTarget::getColorTextureId);
 
-        if (this.inTarget instanceof MainTarget) {
-            this.inTarget.bindRead();
-        }
-
-        for (int i = 0; i < this.auxAssets.size(); ++i) {
+        for(int i = 0; i < this.auxAssets.size(); ++i) {
             this.effect.setSampler(this.auxNames.get(i), this.auxAssets.get(i));
-            this.effect.safeGetUniform("AuxSize" + i).set((float) this.auxHeights.get(i), (float) this.auxWidths.get(i)); // swapped width with height
+            this.effect.safeGetUniform("AuxSize" + i).set((float) this.auxWidths.get(i), (float) this.auxHeights.get(i));
         }
 
         this.effect.safeGetUniform("ProjMat").set(this.shaderOrthoMatrix);
-        this.effect.safeGetUniform("InSize").set((float)this.inTarget.height, (float)this.inTarget.width); // swapped width with height
+        this.effect.safeGetUniform("InSize").set((float)this.inTarget.width, (float)this.inTarget.height);
         this.effect.safeGetUniform("OutSize").set(g, h);
         this.effect.safeGetUniform("Time").set(f);
         Minecraft minecraft = Minecraft.getInstance();
-        this.effect.safeGetUniform("ScreenSize").set((float)minecraft.getWindow().getHeight(), (float)minecraft.getWindow().getWidth()); // swapped width with height
+        this.effect.safeGetUniform("ScreenSize").set((float)minecraft.getWindow().getWidth(), (float)minecraft.getWindow().getHeight());
 
         this.outTarget.clear(Minecraft.ON_OSX);
         this.outTarget.bindWrite(false);
 
-        VRenderSystem.disableCull();
-        RenderSystem.depthFunc(519);
-
-        Renderer.setViewport(0, this.outTarget.width, this.outTarget.height, -this.outTarget.width); // swapped width with height
-        Renderer.resetScissor();
-
         this.effect.apply();
+
+        RenderSystem.depthFunc(519);
 
         BufferBuilder bufferBuilder = Tesselator.getInstance().getBuilder();
         bufferBuilder.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION);
         bufferBuilder.vertex(0.0, 0.0, 500.0).endVertex();
-        bufferBuilder.vertex(h, 0.0, 500.0).endVertex(); // swapped width with height
-        bufferBuilder.vertex(h, g, 500.0).endVertex(); // swapped width with height
-        bufferBuilder.vertex(0.0, g, 500.0).endVertex(); // swapped width with height
+        bufferBuilder.vertex(g, 0.0, 500.0).endVertex();
+        bufferBuilder.vertex(g, h, 500.0).endVertex();
+        bufferBuilder.vertex(0.0, h, 500.0).endVertex();
         BufferUploader.draw(bufferBuilder.end());
         RenderSystem.depthFunc(515);
 
@@ -98,6 +87,5 @@ public class PostPassM {
             }
         }
 
-        VRenderSystem.enableCull();
     }
 }
