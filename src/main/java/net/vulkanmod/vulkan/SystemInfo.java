d5ee99c510aefa59afb06c6fd9c948e9b5e3bcd1
Shadow <saintplayzph69@gmail.com>
2024-06-26 10:57:42 +0800

    Implement processor name reading for mobile devices

--- src/main/java/net/vulkanmod/vulkan/SystemInfo.java
+++ src/main/java/net/vulkanmod/vulkan/SystemInfo.java
@@ -1,12 +1,41 @@
 package net.vulkanmod.vulkan;
 
+import net.vulkanmod.Initializer;
 import oshi.hardware.CentralProcessor;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class SystemInfo {
     public static final String cpuInfo;
 
     static {
+        cpuInfo = isRunningOnAndroid() ? getProcessorNameForAndroid() : getProcessorNameForDesktop();
+    }
+
+    public static String getProcessorNameForAndroid() {
+        try (BufferedReader br = new BufferedReader(new FileReader("/proc/cpuinfo"))) {
+            return br.lines()
+                    .filter(line -> line.startsWith("Hardware"))
+                    .map(line -> line.split(":\\s+", 2)[1])
+                    .findFirst()
+                    .orElse("Unknown");
+        } catch (IOException e) {
+            Initializer.LOGGER.error("Can't obtain your Mobile processor name!", e);
+            return "Unknown";
+        }
+    }
+
+    public static String getProcessorNameForDesktop() {
         CentralProcessor centralProcessor = new oshi.SystemInfo().getHardware().getProcessor();
         cpuInfo = String.format("%s", centralProcessor.getProcessorIdentifier().getName()).replaceAll("\\s+", " ");
+    }
+
+    private static boolean isRunningOnAndroid() {
+        return System.getenv("POJAV_ENVIRON") != null ||
+               System.getenv("SCL_ENVIRON") != null ||
+               System.getenv("POJAV_RENDERER") != null ||
+               System.getenv("SCL_RENDERER") != null;
     }
 }
