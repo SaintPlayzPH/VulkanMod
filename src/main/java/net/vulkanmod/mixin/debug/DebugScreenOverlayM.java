33c890a047a36c77e74c7e4d140ffc8b1cd0730b
Shadow <saintplayzph69@gmail.com>
2024-07-09 13:10:10 +0800

    Update DebugScreenOverlayM.java

--- src/main/java/net/vulkanmod/mixin/debug/DebugScreenOverlayM.java
+++ src/main/java/net/vulkanmod/mixin/debug/DebugScreenOverlayM.java
@@ -100,9 +100,9 @@ public abstract class DebugScreenOverlayM {
         }
         strings.add("");
         strings.add("§d(Vulkan Queue Families)§r");
-        strings.add("Present Queue: " + QueueFamilyIndices.presentFamily != 0 ? "Supported" : "Fallback");
-        strings.add("Graphics Queue: " + QueueFamilyIndices.graphicsFamily != 0 ? "Supported" : "Fallback");
-        strings.add("Transfer Queue: " + QueueFamilyIndices.transferFamily != QueueFamilyIndices.graphicsFamily ? "Supported" : "Fallback");
+        strings.add("Present Queue: " + (QueueFamilyIndices.presentFamily != 0 ? "Supported" : "Fallback"));
+        strings.add("Graphics Queue: " + (QueueFamilyIndices.graphicsFamily != 0 ? "Supported" : "Fallback"));
+        strings.add("Transfer Queue: " + (QueueFamilyIndices.transferFamily != QueueFamilyIndices.graphicsFamily ? "Supported" : "Fallback"));
         
         return strings;
     }
