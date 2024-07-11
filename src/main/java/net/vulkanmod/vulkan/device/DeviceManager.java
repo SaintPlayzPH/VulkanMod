01ebac1d5c01a5899fe7d043d228c57d1efbd56d
Shadow <saintplayzph69@gmail.com>
2024-07-11 21:30:14 +0800

    Update DeviceManager.java

--- src/main/java/net/vulkanmod/vulkan/device/DeviceManager.java
+++ src/main/java/net/vulkanmod/vulkan/device/DeviceManager.java
@@ -23,9 +23,6 @@ import static org.lwjgl.system.MemoryStack.stackPush;
 import static org.lwjgl.vulkan.EXTDebugUtils.VK_EXT_DEBUG_UTILS_EXTENSION_NAME;
 import static org.lwjgl.vulkan.KHRSurface.*;
 import static org.lwjgl.vulkan.VK10.*;
-import static org.lwjgl.vulkan.VK11.VK_API_VERSION_1_1;
-import static org.lwjgl.vulkan.VK12.VK_API_VERSION_1_2;
-import static org.lwjgl.vulkan.VK13.VK_API_VERSION_1_3;
 
 public abstract class DeviceManager {
     public static List<Device> availableDevices;
@@ -220,9 +217,13 @@ public abstract class DeviceManager {
             int res = vkCreateDevice(physicalDevice, createInfo, null, pDevice);
             Vulkan.checkResult(res, "Failed to create logical device");
 
-            vkDevice = new VkDevice(pDevice.get(0), physicalDevice, createInfo, VK_API_VERSION_1_1);
+            vkDevice = new VkDevice(pDevice.get(0), physicalDevice, createInfo, getInstanceVersion());
         }
     }
+
+    private static int getInstanceVersion() {
+        return Device.instanceVersion;
+     }
 
     private static PointerBuffer getRequiredExtensions() {
         PointerBuffer glfwExtensions = glfwGetRequiredInstanceExtensions();
