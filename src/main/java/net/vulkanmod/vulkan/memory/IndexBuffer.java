3870bc432cebcc0310bf17b06ae3a0f554f36cd2
Shadow <saintplayzph69@gmail.com>
2024-07-16 06:18:20 +0800

    Update IndexBuffer.java

--- src/main/java/net/vulkanmod/vulkan/memory/IndexBuffer.java
+++ src/main/java/net/vulkanmod/vulkan/memory/IndexBuffer.java
@@ -2,12 +2,15 @@ package net.vulkanmod.vulkan.memory;
 
 import java.nio.ByteBuffer;
 
-import static org.lwjgl.vulkan.VK10.VK_BUFFER_USAGE_INDEX_BUFFER_BIT;
+import static org.lwjgl.vulkan.VK10.*;
 
 public class IndexBuffer extends Buffer {
 
-    public IndexBuffer(int size, MemoryType type) {
+    public IndexType indexType;
+    
+    public IndexBuffer(int size, MemoryType type, IndexType indexType) {
         super(VK_BUFFER_USAGE_INDEX_BUFFER_BIT, type);
+        this.indexType = indexType;
         this.createBuffer(size);
     }
 
@@ -23,19 +26,16 @@ public class IndexBuffer extends Buffer {
         }
     }
 
-    private void resizeBuffer(int newSize) {
-        this.getType().freeBuffer(this);
-        this.createBuffer(newSize);
-    }
-
     public enum IndexType {
-        SHORT(2),
-        INT(4);
+        SHORT(2, VK_INDEX_TYPE_UINT16),
+        INT(4, VK_INDEX_TYPE_UINT32);
 
         public final int size;
+        public final int type;
 
-        IndexType(int size) {
+        IndexType(int size, int type) {
             this.size = size;
+            this.type = type;
         }
     }
 }
