ff90b90dfc11431da658119eb8049d10cbf52a90
Shadow <saintplayzph69@gmail.com>
2024-07-24 17:16:10 +0800

    Update TerrainRenderType.java

--- src/main/java/net/vulkanmod/render/vertex/TerrainRenderType.java
+++ src/main/java/net/vulkanmod/render/vertex/TerrainRenderType.java
@@ -2,25 +2,39 @@ package net.vulkanmod.render.vertex;
 
 import net.minecraft.client.renderer.RenderType;
 import net.vulkanmod.interfaces.ExtendedRenderType;
+import net.vulkanmod.vulkan.VRenderSystem;
 
 import java.util.EnumSet;
 
 public enum TerrainRenderType {
-    SOLID(262144 /*BIG_BUFFER_SIZE*/),
-    CUTOUT_MIPPED(262144 /*MEDIUM_BUFFER_SIZE*/),
-    CUTOUT(131072 /*SMALL_BUFFER_SIZE*/),
-    TRANSLUCENT(131072 /*SMALL_BUFFER_SIZE*/),
-    TRIPWIRE(131072 /*SMALL_BUFFER_SIZE*/);
+    SOLID(0.0f),
+    CUTOUT_MIPPED(0.5f),
+    CUTOUT(0.1f),
+    TRANSLUCENT(0.0f),
+    TRIPWIRE(0.1f);
 
     public static final TerrainRenderType[] VALUES = TerrainRenderType.values();
 
     public static final EnumSet<TerrainRenderType> COMPACT_RENDER_TYPES = EnumSet.of(CUTOUT_MIPPED, TRANSLUCENT);
     public static final EnumSet<TerrainRenderType> SEMI_COMPACT_RENDER_TYPES = EnumSet.of(CUTOUT_MIPPED, CUTOUT, TRANSLUCENT);
 
-    public final int initialSize;
+    static {
+        SEMI_COMPACT_RENDER_TYPES.add(CUTOUT);
+        SEMI_COMPACT_RENDER_TYPES.add(CUTOUT_MIPPED);
+        SEMI_COMPACT_RENDER_TYPES.add(TRANSLUCENT);
 
-    TerrainRenderType(int initialSize) {
-        this.initialSize = initialSize;
+        COMPACT_RENDER_TYPES.add(CUTOUT_MIPPED);
+        COMPACT_RENDER_TYPES.add(TRANSLUCENT);
+    }
+
+    public final float alphaCutout;
+
+    TerrainRenderType(float alphaCutout) {
+        this.alphaCutout = alphaCutout;
+    }
+
+    public void setCutoutUniform() {
+        VRenderSystem.alphaCutout = this.alphaCutout;
     }
 
     public static TerrainRenderType get(RenderType renderType) {
