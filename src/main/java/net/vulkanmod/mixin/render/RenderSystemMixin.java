0196a93fc9dc1b53abf7730e39762a771003a2f7
Shadow <saintplayzph69@gmail.com>
2024-07-26 05:52:21 +0800

    Update RenderSystemMixin.java

--- src/main/java/net/vulkanmod/mixin/render/RenderSystemMixin.java
+++ src/main/java/net/vulkanmod/mixin/render/RenderSystemMixin.java
@@ -23,6 +23,8 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 import java.util.function.Consumer;
 
