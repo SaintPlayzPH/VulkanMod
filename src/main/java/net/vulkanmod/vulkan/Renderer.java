9ddaf49f92a08cfce99aa17f505aa4197317fd1c
Shadow <saintplayzph69@gmail.com>
2024-07-29 18:16:43 +0800

    Update Renderer.java

--- src/main/java/net/vulkanmod/vulkan/Renderer.java
+++ src/main/java/net/vulkanmod/vulkan/Renderer.java
@@ -386,6 +386,7 @@ public class Renderer {
 
         WorldRenderer.getInstance().uploadSections();
         UploadManager.INSTANCE.submitUploads();
+        UploadManager.INSTANCE.syncUploads();
     }
 
     public void addUsedPipeline(Pipeline pipeline) {
@@ -437,6 +438,7 @@ public class Renderer {
 
         if (framesNum != newFramesNum) {
             UploadManager.INSTANCE.submitUploads();
+            UploadManager.INSTANCE.syncUploads();
 
             framesNum = newFramesNum;
             MemoryManager.createInstance(newFramesNum);
